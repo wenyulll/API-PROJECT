c8d1fe070d97e6bbef0bf9677c0bc9d8c68a6af6
wenyulll <wenyu414@gmail.com>
2023-06-12 14:23:57 -0700

    add demo-spot

--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      // define association here\
+      SpotImage.belongsTo(models.Spot, { foreignKey: "spotId" });
     }
   }
   SpotImage.init({
---
+++ backend/db/seeders/20230612202339-demo-spot.js
@@ -0,0 +1,82 @@
+'use strict';
+// const bcrypt = require("bcryptjs");
+
+// let options = {};
+// if (process.env.NODE_ENV === 'production') {
+//   options.schema = process.env.SCHEMA;  // define your schema in options object
+// }
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Spots', [
+      {
+        "ownerId": 1,
+        "address": "865 Wines Lane",
+        "city": "Houston",
+        "state": "Texas",
+        "country": "United States",
+        "lat": 30.051216,
+        "lng": -95.390831,
+        "name": "Walk to the Beach from this Ocean Front Home",
+        "description": "Your beachfront escape awaits you.",
+        "price": 200
+      },
+      {
+        "ownerId": 2,
+        "address": "867 Wines Lane",
+        "city": "Houston",
+        "state": "Texas",
+        "country": "United States",
+        "lat": 30.051216,
+        "lng": -95.390831,
+        "name": "Bellacollina Farms Beautiful Briones Retreat",
+        "description": "Beautiful Briones ~ Bay Area Retreat.",
+        "price": 200
+      },
+      {
+        "ownerId": 3,
+        "address": "2655 285 Stratford Park",
+        "city": "Evansville",
+        "state": "Indiana",
+        "country": "United States",
+        "lat": 38.029121,
+        "lng": -87.584869,
+        "name": "The Little House",
+        "description": "The Little House is a quaint place to stay during your time in the South Jersey area .",
+        "price": 199
+      },
+      {
+        "ownerId": 4,
+        "address": "4797 Chestnut Street",
+        "city": "Tampa",
+        "state": "Florida",
+        "country": "United States",
+        "lat": 28.022614,
+        "lng": -82.455887,
+        "name": "Walk to the Beach from this Ocean Front Home",
+        "description": "Your beachfront escape awaits you.",
+        "price": 198
+      },
+      {
+        "ownerId": 5,
+        "address": "1475 Lee Avenue",
+        "city": "Camden",
+        "state": "New Jersey",
+        "country": "United States",
+        "lat": 39.897949,
+        "lng": -75.044334,
+        "name": "Sean's Homestead,the Green Room",
+        "description": "YBeautiful bedroom in an old world in the heart of historic Jersey City",
+        "price": 197
+      },
+    ], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Spots', {
+      city: { [Op.in]: ['Camden', 'Tampa', 'Evansville'] }
+    }, {});
+  }
+};
